68963ceab2b01115954962742007a989a5413993
mark-ye <loading23@126.com>
2017-07-06 15:23:35 +0800

    project-helloworld
    2017-07-06

---
+++ project/helloworld/express_demo.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+ 
+app.get('/', function (req, res) {
+   res.send('Hello World ');
+})
+ 
+var server = app.listen(8000, function () {
+ 
+  var host = server.address().address;
+  var port = server.address().port;
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port);
+  console.log(server.address());
+ 
+})
